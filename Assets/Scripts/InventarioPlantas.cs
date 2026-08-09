f2ff9c1a1ad4f1a0aa447ab2bbfac60cd2d832c1
matiasquintana <matiad12342@gmail.com>
2026-08-09 16:43:23 -0300

    animacionRecojerPlantas

--- Assets/Scripts/InventarioPlantas.cs
+++ Assets/Scripts/InventarioPlantas.cs
@@ -7,15 +7,15 @@ public partial class InventarioPlantas
 
    public void RecibirPlanta(Planta planta)
    {
-      _plantaActual = planta;
+	  _plantaActual = planta;
    }
    public Color ObtenerColorPlantaActual()
    {
-      if (_plantaActual == null)
-      {
-         throw new InvalidOperationException("No hay una planta actual en el inventario.");
-      }
+	  if (_plantaActual == null)
+	  {
+		 throw new InvalidOperationException("No hay una planta actual en el inventario.");
+	  }
 
-      return _plantaActual.ObtenerColor();
+	  return _plantaActual.ObtenerColor();
    }
 }
