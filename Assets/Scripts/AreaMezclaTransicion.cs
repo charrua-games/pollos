3316d95e13f8b92bc744d1ca358c7dfa6537416d
NizIgnacio <nizignacio514@gmail.com>
2026-08-09 00:51:12 -0300

    Se mueve minijuego de ritmo a Scenes/Minjuegos.. Se empieza a armar el juego, faltan sprites y dejarlo lindo en general

--- Assets/Scripts/AreaMezclaTransicion.cs
+++ Assets/Scripts/AreaMezclaTransicion.cs
@@ -1,12 +1,15 @@
 using Godot;
 using System;
 
-public partial class BotonInicio : Button
+public partial class AreaMezclaTransicion : Button
 {
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Pressed += OnPressed;
 	}
+
+
 	private async void OnPressed()
 	{
 		await CanvasTransition.Instance.ChangeSceneAsync("res://Assets/Scenes/Overworld.tscn");
---
+++ Assets/Scripts/ButtonTransitionScript.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class ButtonTransitionScript : Button
+{
+	[Export]
+	public PackedScene EscenaDestino;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Pressed += OnPressed;
+	}
+
+	private async void OnPressed()
+	{
+		await CanvasTransition.Instance.ChangeSceneAsync(EscenaDestino.ResourcePath);
+	}
+}
