2e3030ae346b0ee6dabace7cc78c989a1e77072e
NizIgnacio <nizignacio514@gmail.com>
2026-08-09 12:38:23 -0300

    Se arregla bug de transiciones cambiando Export de 'PackedScene' a string

--- Assets/Scripts/ButtonTransitionScript.cs
+++ Assets/Scripts/ButtonTransitionScript.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class ButtonTransitionScript : Button
 {
-	[Export]
-	public PackedScene EscenaDestino;
+	[Export(PropertyHint.File, "*.tscn")]
+	public string EscenaDestino;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,6 +13,18 @@ public partial class ButtonTransitionScript : Button
 
 	private async void OnPressed()
 	{
-		await CanvasTransition.Instance.ChangeSceneAsync(EscenaDestino.ResourcePath);
+		if (string.IsNullOrEmpty(EscenaDestino))
+		{
+			GD.PrintErr($"{Name}: EscenaDestino no está asignado.");
+			return;
+		}
+
+		if (CanvasTransition.Instance == null || !IsInstanceValid(CanvasTransition.Instance))
+		{
+			GD.PrintErr($"{Name}: CanvasTransition.Instance no es válido.");
+			return;
+		}
+
+		await CanvasTransition.Instance.ChangeSceneAsync(EscenaDestino);
 	}
 }
